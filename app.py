2d80eca4e9a3e8c22502f3533b6bddd04d2f6329
Pratikshya Mishra <mishrapratikshya12@gmail.com>
2020-12-28 00:00:14 +0530

    'Email-SMS-spam-detection'

---
+++ app.py
@@ -0,0 +1,24 @@
+import nltk
+import joblib
+from nltk.corpus import stopwords
+import streamlit as st
+predictions = joblib.load('predictions_joblib')
+cv = joblib.load('cv_joblib')
+clf = joblib.load('clf_joblib')
+
+def predict(raw_text):
+	vectors = cv.transform(raw_text)
+	return clf.predict(vectors)
+
+def main():
+	st.title("Spam Detector")
+	raw_text = st.text_input("Enter the message","Type Here..")
+	submit = st.button("Check spam")
+	if submit:
+		result = predict([raw_text])
+		st.success(result)
+	    
+
+       
+if __name__ == '__main__':
+    main()
